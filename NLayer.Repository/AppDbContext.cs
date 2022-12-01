f52621793df87308444a8ee00ec8d4c47ca3d42f
umutSahinkaya <umutsahinkaya1@gmail.com>
2022-12-01 18:08:20 +0300

    NLayer.Repository is Okay

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+using NLayer.Repository.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opts) : base(opts)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //Seed Tarafından yaptıklarım buradan da yapılabilir fakat buradaki kod kalabalıgını azaltmak için Seeds oluşturduk.
+            modelBuilder.Entity<ProductFeature>().HasData(
+                new ProductFeature
+                {
+                    Id = 1,
+                    Color = "Kırmızı",
+                    Height=100,
+                    Width=200,
+                    ProductId=1,
+                }, new ProductFeature
+                {
+                    Id = 2,
+                    Color = "Sarı",
+                    Height = 300,
+                    Width = 350,
+                    ProductId = 2,
+                });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product
+                {
+                    Id = 1,
+                    CategoryId = 1,
+                    Name = "Kitap 1",
+                    Price = 100,
+                    Stock = 20,
+                    CreatedDate = DateTime.Now,
+
+                }, new Product
+                {
+                    Id = 2,
+                    CategoryId = 1,
+                    Name = "Kitap 2",
+                    Price = 200,
+                    Stock = 30,
+                    CreatedDate = DateTime.Now,
+
+                }
+                , new Product
+                {
+                    Id = 3,
+                    CategoryId = 1,
+                    Name = "Kitap 3",
+                    Price = 600,
+                    Stock = 60,
+                    CreatedDate = DateTime.Now,
+
+                }, new Product
+                {
+                    Id = 4,
+                    CategoryId = 2,
+                    Name = "Kalem 1",
+                    Price = 50,
+                    Stock = 60,
+                    CreatedDate = DateTime.Now,
+
+                }, new Product
+                {
+                    Id = 5,
+                    CategoryId = 2,
+                    Name = "Kalem 2",
+                    Price = 500,
+                    Stock = 20,
+                    CreatedDate = DateTime.Now,
+
+                });
+        }
+    }
+}
