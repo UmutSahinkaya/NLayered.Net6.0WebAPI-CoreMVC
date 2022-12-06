c142e0b01deaa545dd394bb2e20bf99a899d7449
umutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2022-12-06 17:33:48 +0300

    Service Layer is Completed

--- NLayer.Core/Repositories/IGenericRepository.cs
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -5,7 +5,7 @@ namespace NLayer.Core.Repositories
     public interface IGenericRepository<T> where T : class
     {
         Task<T> GetByIdAsync(int id);
-        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+        IQueryable<T> GetAll();
         //productRepository.where(x=>x.id>5).OrderBy??.ToListAsync();
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
--- NLayer.Core/Services/IService.cs
+++ NLayer.Core/Services/IService.cs
@@ -10,12 +10,12 @@ namespace NLayer.Core.Services
     public interface IService<T> where T : class
     {
         Task<T> GetByIdAsync(int id);
-        Task<IQueryable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllAsync();
         //productRepository.where(x=>x.id>5).OrderBy??.ToListAsync();
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
-        Task AddAsync(T entity);
-        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<T> AddAsync(T entity);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
         Task UpdateAsync(T entity);
         Task RemoveAsync(T entity);
         Task RemoveRangeAsync(IEnumerable<T> entities);
