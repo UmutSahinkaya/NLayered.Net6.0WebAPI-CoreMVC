222f5d5e6bbad9e87bd04016505b7910193ae8e3
umutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2022-12-12 21:03:51 +0300

    Caching is Ok.

--- NLayer.API/Modules/RepoServiceModule.cs
+++ NLayer.API/Modules/RepoServiceModule.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using NLayer.Caching;
 using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
@@ -27,6 +28,9 @@ namespace NLayer.API.Modules
             builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x=>x.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerLifetimeScope();
 
             builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<ProductServiceWithCaching>().As<IProductService>();
+
+
             //InstancePerLifetimeScope => Scope(Mvc)
             //InstancePerDependency => transiet
             
